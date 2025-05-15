b356b56240cc9dc05e0d62f33c55b5c7715cebf9
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-05-15 11:43:10 +0200

    whisper.cpp

---
+++ src/routes/whisper/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  //import createModule from "@transcribe/shout";
+  import { onMount } from "svelte";
+  //import type { FileTranscriber } from "@transcribe/transcriber";
+
+  let transcriber: any;
+
+  onMount(async () => {
+    const createModuleImport = await import("@transcribe/shout");
+    // create new instance
+    /*
+    ip.FileTranscriber.transcriber = new ip.FileTranscriber({
+      createModule,
+      model: "/ggml-tiny-q5_1.bin",
+    });
+
+    // and initialize the transcriber
+    await transcriber.init();
+    */
+  });
+</script>
+
+123
