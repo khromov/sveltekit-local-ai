a31149edbecadc23f96424b5acb90bc54eddc514
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-08-23 22:45:25 +0200

    wip

---
+++ src/routes/(apps)/count-tokens/anthropic-claude/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	const { data } = $props();
+	let content: number[] = $state([]);
+	onMount(() => {
+		if (data.encoder) {
+			console.log('Encoding with', data.encoder);
+			content = data.encoder.encode('hello world');
+		}
+	});
+</script>
+
+{JSON.stringify(content)}
