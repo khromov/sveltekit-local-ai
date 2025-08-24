3896394ef1d3df11469b82d4f297a5c49149cb97
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-08-24 23:37:14 +0200

    Update +page.svelte

--- src/routes/(apps)/text-to-speech/+page.svelte
+++ src/routes/(apps)/text-to-speech/+page.svelte
@@ -790,19 +790,6 @@
 		height: 1.25rem;
 	}
 
-	.primary-action-btn :global(.lucide-loader) {
-		animation: spin 1s linear infinite;
-	}
-
-	@keyframes spin {
-		from {
-			transform: rotate(0deg);
-		}
-		to {
-			transform: rotate(360deg);
-		}
-	}
-
 	.hidden {
 		display: none;
 	}
