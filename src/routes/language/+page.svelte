59e7e7b2f32bee19a88b63a6904693e1ab4d9f27
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-08-25 16:14:21 +0200

    wip

--- src/routes/language/+page.svelte
+++ src/routes/language/+page.svelte
@@ -14,24 +14,7 @@
 	const availableLanguages: Language[] = [
 		{ code: 'en', name: 'English', nativeName: 'English' },
 		{ code: 'es', name: 'Spanish', nativeName: 'Español' },
-		{ code: 'fr', name: 'French', nativeName: 'Français' },
-		{ code: 'de', name: 'German', nativeName: 'Deutsch' },
-		{ code: 'it', name: 'Italian', nativeName: 'Italiano' },
-		{ code: 'pt', name: 'Portuguese', nativeName: 'Português' },
-		{ code: 'ru', name: 'Russian', nativeName: 'Русский' },
-		{ code: 'ja', name: 'Japanese', nativeName: '日本語' },
-		{ code: 'ko', name: 'Korean', nativeName: '한국어' },
-		{ code: 'zh', name: 'Chinese', nativeName: '中文' },
-		{ code: 'ar', name: 'Arabic', nativeName: 'العربية' },
-		{ code: 'hi', name: 'Hindi', nativeName: 'हिन्दी' },
-		{ code: 'nl', name: 'Dutch', nativeName: 'Nederlands' },
-		{ code: 'sv', name: 'Swedish', nativeName: 'Svenska' },
-		{ code: 'da', name: 'Danish', nativeName: 'Dansk' },
-		{ code: 'no', name: 'Norwegian', nativeName: 'Norsk' },
-		{ code: 'fi', name: 'Finnish', nativeName: 'Suomi' },
-		{ code: 'pl', name: 'Polish', nativeName: 'Polski' },
-		{ code: 'tr', name: 'Turkish', nativeName: 'Türkçe' },
-		{ code: 'cs', name: 'Czech', nativeName: 'Čeština' }
+		{ code: 'ja', name: 'Japanese', nativeName: '日本語' }
 	];
 
 	let selectedLanguage = $state('en');
