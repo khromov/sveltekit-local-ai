482ccff1a25a8ca7d82b2796934ff25e6a29cab9
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-08-26 00:40:30 +0200

    wip

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,11 @@
 import { loadCatalog, loadIDs, key } from './locales/loader.svelte.js';
 import { runWithLocale, loadLocales } from 'wuchale/load-utils/server';
-// import { locales } from 'virtual:wuchale/locales';
+import { locales } from 'virtual:wuchale/locales';
 
-// Load only the english one in SSR
-await loadLocales(key, loadIDs, loadCatalog, ['en']);
+await loadLocales(key, loadIDs, loadCatalog, locales);
 
 export const handle = async ({ event, resolve }) => {
-	const defaultLocale = 'en';
-	return await runWithLocale(defaultLocale, () => resolve(event));
+	const localeToLoad =
+		event.params.lang && locales.includes(event.params.lang) ? event.params.lang : 'en';
+	return await runWithLocale(localeToLoad, () => resolve(event));
 };
